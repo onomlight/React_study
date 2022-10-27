1d7af4b82940b0ac418a460a497c58babf6bd4bf
onomlight <pass041@nate.com>
2022-10-27 23:07:51 +0900

    fun / const 해도 무관함

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,9 @@ import Card from "./Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,13 +13,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button
-        onClick={() => {
-          console.log("clicked");
-        }}
-      >
-        Change Title
-      </button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
