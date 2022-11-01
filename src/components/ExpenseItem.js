3fd2e95ec6698ba07a41076a02a0e1f58c2db50b
onomlight <pass041@nate.com>
2022-11-01 20:49:17 +0900

    title update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.css";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log("react");
   const clickHandler = () => {
-    console.log("clicked");
+    setTitle("Update");
+    //console.log("clicked");
+    console.log(title);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
