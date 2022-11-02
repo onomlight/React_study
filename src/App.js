9e340ac3954f944ac7f1af663b16cbc15ed0d9b8
onomlight <pass041@nate.com>
2022-11-02 18:32:53 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   const expenses = [
@@ -23,6 +26,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   // return React.createElement(
   //   'div',
   //   {},
@@ -32,7 +40,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,17 @@
+import React from "react";
+
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
-import { useState } from "react";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log("react");
-  const clickHandler = () => {
-    setTitle("Update");
-    //console.log("clicked");
-    console.log(title);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
